d61a4c84220c3d03d97d0dc949165c5af731023b
Tengol <tengol@aliyun.com>
2020-04-23 23:03:21 +0800

    423-@ConfigurationProperties 注入属性时增加数据校验

--- boot-01-helloworld/src/main/java/com/tengol/course/spring/boot/boot01/config/PersonProperties.java
+++ boot-01-helloworld/src/main/java/com/tengol/course/spring/boot/boot01/config/PersonProperties.java
@@ -4,10 +4,15 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Email;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+
+
+
 
 /**
  * PersonConfig
@@ -17,11 +22,14 @@ import java.util.Map;
  */
 @Data
 @Component
+@Validated
 @ConfigurationProperties(prefix = "person")
 public class PersonProperties {
     private String name;
     private Integer age;
     private Boolean boss;
+    @Email
+    private String email;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date birthday;
